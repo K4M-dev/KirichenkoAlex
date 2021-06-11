96b1f060c18ba8ab2a255605286c5f3e21744d3f
Alex <yashik.alex2951@mail.ru>
2021-06-11 13:09:06 +0300

    Функция вычисляет разность между двумя датами в днях.

---
+++ Alex_5/lab5/lab5/main.cpp
@@ -0,0 +1,48 @@
+#include "DiffBetweenDate.h"
+int main() {
+	setlocale(LC_ALL, "Russian");
+	Date FirstDate(-2002, 9, 14);
+	Date SecondDate(2021, 5, -29);
+
+	try {
+		cout << DiffBetweenDate1(FirstDate, SecondDate) << endl;
+	}
+	catch(...) {
+		cout << "Дата введена неправильно,без спецификатора" << endl;
+	}
+
+	try {
+		cout << DiffBetweenDate2(FirstDate, SecondDate) << endl;
+	}
+	catch (int) {
+		cout << "Дата введена неправильно,спецификатор throw" << endl;
+	}
+
+	try {
+		cout << DiffBetweenDate3(FirstDate, SecondDate) << endl;
+	}
+	catch (invalid_argument error) {
+		cerr << error.what() << endl;
+	}
+
+	try {
+		cout << DiffBetweenDate4(FirstDate, SecondDate) << endl;
+	}
+	catch (Exception) {
+		cerr << "Дата введена неправильно,пустой класс" << endl;
+	}
+
+	try {
+		cout << DiffBetweenDate5(FirstDate, SecondDate) << endl;
+	}
+	catch (Exception2 error) {
+		cerr << error.message << endl;
+	}
+
+	try {
+		cout << DiffBetweenDate6(FirstDate, SecondDate) << endl;
+	}
+	catch (Exception3 error) {
+		cerr << error.what() << endl;
+	}
+}
